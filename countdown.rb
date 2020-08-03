cdb2357a4305b4b7f46451b21cdd69f497e28123
Yoshi Mejia <yoshimejia00@gmail.com>
2020-08-03 02:50:17 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,13 @@ def countdown(number)
 
   return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    sleep(1.second)
+  end
+
+  return "HAPPY NEW YEAR!"
+end
